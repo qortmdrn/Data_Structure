a9242dbbceb5e4e5af44d7b81ebd1f28d9a9f6d5
Seungwoo BAEK <ggp07190@gmail.com>
2025-05-12 19:18:35 +0900

    fix: list stack queue

---
+++ DS_MidTerm/src/BinaryTreeNode.java
@@ -0,0 +1,26 @@
+// ADT for binary tree nodes
+public interface BinaryTreeNode<E> {
+    // Return and set the element value
+    public E element();
+    public E setElement(E v);
+
+    // Return the left child and the right child
+    public BinaryTreeNode<E> left();
+    public BinaryTreeNode<E> right();
+
+    // Return whether the node is leaf or not
+    public boolean isLeaf();
+
+    default void preOrderTraversal(BinaryTreeNode root) {
+        if (root == null) return;
+        visit(root);
+        preOrderTraversal(root.left());
+        preOrderTraversal(root.right());
+    };
+
+    default void preOrderTraversal2(BinaryTreeNode root) {
+        visit(root);
+        if(root.left() != null) preOrderTraversal2(root.left());
+        if(root.right() != null) preOrderTraversal2(root.right());
+    }
+}
---
+++ DS_MidTerm/src/VarBinNode.java
@@ -0,0 +1,29 @@
+// Base class
+public interface VarBinNode {
+    public boolean isLeaf();
+}
+
+// Leaf Node
+class VarLeafNode implements VarBinNode {
+    private String operand;
+    public VarLeafNode(String val) { operand = val; }
+    public boolean isLeaf() { return true; }
+    public String value() { return operand; }
+}
+
+// Internal Node
+class VarIntlNode implements VarBinNode {
+    private VarBinNode left;
+    private VarBinNode right;
+    private Character operator;
+
+    public VarIntlNode(Character op, VarBinNode l, VarBinNode r) {
+        operator = op;
+        left = l;
+        right = r;
+    }
+    public boolean isLeaf() { return false; }
+    public VarBinNode leftChild() { return left; }
+    public VarBinNode rightChild() { return right; }
+    public Character value() { return operator; }
+}
